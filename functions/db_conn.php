7738eae32fcd4a3e5963d4314828f746aca2cc84
steveokay <mokaysteve@gmail.com>
2017-07-13 22:36:04 +0300

    Update db_conn.php

--- functions/db_conn.php
+++ functions/db_conn.php
@@ -1,8 +1,8 @@
   <?php
-  define("DB_SERVER","localhost");
-  define("DB_USER","root");
+  define("DB_SERVER","");
+  define("DB_USER","");
   define("DB_PASS","");
-  define("DB_NAME","seasons_basket");
+  define("DB_NAME","");
   
   //1. create a database connection
   $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
